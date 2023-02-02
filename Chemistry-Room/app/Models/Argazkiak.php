5ad5d453a73706136407be43af0702e46c795548
DavidR <davidrodri1602@gmail.com>
2023-02-02 09:30:19 +0100

    4.5

--- Chemistry-Room/app/Models/Argazkiak.php
+++ Chemistry-Room/app/Models/Argazkiak.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+      
 class argazkiak extends Model
 {
     use HasFactory;
